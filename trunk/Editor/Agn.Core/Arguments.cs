65ab4eb34eacbdced84bb5639749fe1e4d765a96
jdberube@gmail.com <jdberube@gmail.com@a8cd0d33-2a72-5bf5-5b35-c9dba72e4cff>
2012-12-23 08:47:41 +0000

    Added argument parsing, and by default turned off the display for the console.

--- trunk/Editor/Agn.Core/Arguments.cs
+++ trunk/Editor/Agn.Core/Arguments.cs
@@ -1,5 +1,8 @@
-﻿
-using log4net;
+﻿using log4net;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace Agn
 {
     /// <summary>
@@ -13,13 +16,35 @@ namespace Agn
         {
             this.Args = args;
 
-            if (this.log.IsInfoEnabled)
+            this.Flags = args.Select(a => ProcessMatch(a))
+                             .Where(t => t != null)
+                             .ToLookup(t => t.Item1, t => t.Item2);
+        }
+
+        private Tuple<string, string> ProcessMatch(string arg)
+        {
+            this.log.Info("Parsing Arg: " + arg);
+            Match match = flagExpression.Match(arg);
+            if (match.Success)
             {
-                foreach (var arg in args)
+                // Look up the flag always, and sometimes the value.
+                string flag = match.Groups["Flag"].Value;
+                string value = string.Empty;
+                if (match.Groups["Value"].Success)
                 {
-                    this.log.Info("Parsing Arg: " + arg);
-                    // TODO convert args.
+                    value = match.Groups["Value"].Value;
                 }
+
+                this.log.Info("Flag: " + flag);
+                this.log.Info("Value: " + value);
+
+                // Return the results.
+                return Tuple.Create(flag, value);
+            }
+            else
+            {
+                this.log.Info("Failed");
+                return null;
             }
         }
 
@@ -27,5 +52,12 @@ namespace Agn
         /// Gets the original collection of input arguments.
         /// </summary>
         public string[] Args { get; private set; }
+
+        /// <summary>
+        /// Gets the collection of flags that were parsed.
+        /// </summary>
+        public ILookup<string, string> Flags { get; private set; }
+
+        private static Regex flagExpression = new Regex("(-(?<Flag>[\\w\\?])|--(?<Flag>\\w+)=\"(?<Value>\\w+)\"|--(?<Flag>\\w+)=(?<Value>\\w+)|--(?<Flag>\\w+))", RegexOptions.Compiled);
     }
 }
--- trunk/Editor/Agn.Testing/ArgumentsTest.cs
+++ trunk/Editor/Agn.Testing/ArgumentsTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Agn
@@ -9,11 +10,38 @@ namespace Agn
         [TestMethod]
         public void CtorTest()
         {
-            var args = new[] { "1", "2", "3" };
+            var args = new[] { 
+                "1",
+                "2", 
+                "3",
+                "-a", 
+                "--word", 
+                "--wordandvalue=asdf",
+                "--wordandvalue=\"zxcv\"" };
             var arguments = new Arguments(args);
 
-            // Simple test for now.
+            // Make sure that the arrays are est.
             CollectionAssert.AreEqual(args, arguments.Args);
+
+            // Test the flags.
+            Assert.IsNotNull(arguments.Flags);
+            Assert.AreEqual(3, arguments.Flags.Count());
+
+            // Look up the a flag.
+            var flags = arguments.Flags["a"];
+            Assert.AreEqual(1, flags.Count());
+            Assert.AreEqual(string.Empty, flags.Single());
+
+            // Look up the word flag.
+            flags = arguments.Flags["word"];
+            Assert.AreEqual(1, flags.Count());
+            Assert.AreEqual(string.Empty, flags.Single());
+
+            // Look up the wordandvalue flag.
+            flags = arguments.Flags["wordandvalue"];
+            Assert.AreEqual(2, flags.Count());
+            Assert.AreEqual("asdf", flags.First());
+            Assert.AreEqual("zxcv", flags.Last());
         }
     }
 }
