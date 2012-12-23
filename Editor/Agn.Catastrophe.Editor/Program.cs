b78dc9f3c96757d43b6bde09c78223f3265bcea0
jdberube <jdberube@gmail.com>
2012-12-23 08:50:03 +0000

    Minor change to always default the console to be off.

--- Editor/Agn.Catastrophe.Editor/Program.cs
+++ Editor/Agn.Catastrophe.Editor/Program.cs
@@ -13,6 +13,10 @@ namespace Agn.Catastrophe.Editor
         [STAThread]
         static void Main(string[] args)
         {
+            // Always turn off the console.
+            var console = NativeMethods.GetConsoleWindow();
+            NativeMethods.ShowWindow(console, NativeMethods.SW_HIDE);
+
             // Start the application.
             var starter = new ApplicationStarter(args);
             starter.Start();
