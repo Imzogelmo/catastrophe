b08eb5938e1208be1b89dd34beb206c4e0bbbcd7
jdberube <jdberube@gmail.com>
2012-12-30 17:04:05 +0000

    Initial revision for interop into Rpg-Core.

--- Editor/Agn.Catastrophe.Interop/ResourceManagerWrapper.cpp
+++ Editor/Agn.Catastrophe.Interop/ResourceManagerWrapper.cpp
@@ -3,19 +3,36 @@
 #include "ResourceManagerWrapper.h"
 #include "ITexture.h"
 
+#pragma managed(push, off)
+void set_base_directory(ResourceManager* resource_manager, const char* value) {
+	resource_manager->SetBaseDirectory(value);
+}
+
+void set_font_directory(ResourceManager* resource_manager, const char* value) {
+	resource_manager->SetFontDirectory(value);
+}
+
+void set_texture_directory(ResourceManager* resource_manager, const char* value) {
+	resource_manager->SetTextureDirectory(value);
+}
+
+void unload_texture(ResourceManager* resource_manager, const char* file_name) {
+	resource_manager->UnloadTexture(file_name);
+}
+#pragma managed(pop)
+
 Agn::Catastrophe::ResourceManagerWrapper::ResourceManagerWrapper() {
 	this->_base_directory = gcnew StringWrapper();
 	this->_font_directory = gcnew StringWrapper();
 	this->_texture_directory = gcnew StringWrapper();
+	this->_resource_manager = new ResourceManager();
 }
 
 Agn::Catastrophe::ResourceManagerWrapper::~ResourceManagerWrapper() {
 	delete this->_base_directory;
 	delete this->_font_directory;
 	delete this->_texture_directory;
-}
-
-void Agn::Catastrophe::ResourceManagerWrapper::UpdateBaseDirectory() {
+	delete this->_resource_manager;
 }
 
 String^ Agn::Catastrophe::ResourceManagerWrapper::BaseDirectory::get() {
@@ -24,6 +41,9 @@ String^ Agn::Catastrophe::ResourceManagerWrapper::BaseDirectory::get() {
 
 void Agn::Catastrophe::ResourceManagerWrapper::BaseDirectory::set(String^ value) {
 	this->_base_directory->ManagedValue = value;
+	set_base_directory(
+		this->_resource_manager, 
+		this->_base_directory->NativeValue.c_str());
 }
 
 String^ Agn::Catastrophe::ResourceManagerWrapper::FontDirectory::get() {
@@ -32,6 +52,9 @@ String^ Agn::Catastrophe::ResourceManagerWrapper::FontDirectory::get() {
 
 void Agn::Catastrophe::ResourceManagerWrapper::FontDirectory::set(String^ value) {
 	this->_font_directory->ManagedValue = value;
+	set_font_directory(
+		this->_resource_manager, 
+		this->_font_directory->NativeValue.c_str());
 }
 
 String^ Agn::Catastrophe::ResourceManagerWrapper::TextureDirectory::get() {
@@ -40,21 +63,53 @@ String^ Agn::Catastrophe::ResourceManagerWrapper::TextureDirectory::get() {
 
 void Agn::Catastrophe::ResourceManagerWrapper::TextureDirectory::set(String^ value) {
 	this->_texture_directory->ManagedValue = value;
+	set_texture_directory(
+		this->_resource_manager, 
+		this->_texture_directory->NativeValue.c_str());
 }
 
 void Agn::Catastrophe::ResourceManagerWrapper::DeleteResources() {
+	this->_resource_manager->DeleteResources();
 }
 
 Agn::Catastrophe::ITexture^ Agn::Catastrophe::ResourceManagerWrapper::GetTexture(String^ fileName) {
+	StringWrapper^ wrapper = nullptr;
+	try {
+		wrapper = gcnew StringWrapper(fileName);
+		//this->_resource_manager->GetTexture(wrapper->FcValue);
+	} finally {
+		if (wrapper != nullptr) {
+			delete wrapper;
+		}
+	}
 	return nullptr;
 }
 
 Agn::Catastrophe::ITexture^ Agn::Catastrophe::ResourceManagerWrapper::LoadTexture(String^ fileName) {
+	StringWrapper^ wrapper = nullptr;
+	try {
+		wrapper = gcnew StringWrapper(fileName);
+		//this->_resource_manager->LoadTexture(wrapper->FcValue);
+	} finally {
+		if (wrapper != nullptr) {
+			delete wrapper;
+		}
+	}
 	return nullptr;
 }
 
 void Agn::Catastrophe::ResourceManagerWrapper::ReloadGraphicsResources() {
+	this->_resource_manager->ReloadGraphicsResources();
 }
 
 void Agn::Catastrophe::ResourceManagerWrapper::UnloadTexture(String^ fileName) {
+	StringWrapper^ wrapper = nullptr;
+	try {
+		wrapper = gcnew StringWrapper(fileName);
+		unload_texture(this->_resource_manager, wrapper->NativeValue.c_str());
+	} finally {
+		if (wrapper != nullptr) {
+			delete wrapper;
+		}
+	}
 }
