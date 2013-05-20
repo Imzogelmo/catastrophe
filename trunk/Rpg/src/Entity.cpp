7b8af38e3cfc9388b3c30c61d86d545f76167a7b
arpeggiodragon <arpeggiodragon@a8cd0d33-2a72-5bf5-5b35-c9dba72e4cff>
2013-05-20 09:25:14 +0000

    Switched to a unity-component build for Entities and Screens, as these are likely to comprise the bulk of cpp files otherwise. This simplifies project compatibility, and speeds up compile times greatly.

---
+++ trunk/Rpg/src/Entity.cpp
@@ -0,0 +1,26 @@
+// catastrophe rpg engine
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+
+// The unity-component-build experiment continues! :D
+// All Entities share most of the same code (and headers) so they are compiled into
+// an individual "module" (ie, as a single .obj file) for speed.
+
+#include "Entity.inl"
+#include "ScriptEntity.inl"
+#include "TextEntity.inl"
+#include "FloatyText.inl"
+#include "BouncyText.inl"
+
+#include "Combatant.inl"
+#include "Npc.inl"
+
+
---
+++ trunk/Rpg/src/Screens.cpp
@@ -0,0 +1,24 @@
+// catastrophe rpg engine
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+
+// The unity-component-build experiment continues! :D
+// All screens share 95% of the same code so they are compiled into
+// an individual "module" (ie, as a single .obj file) for speed.
+
+#include "ScreenManager.inl"
+#include "Screen.inl"
+#include "GameScreen.inl"
+#include "MapScreen.inl"
+#include "BattleScreen.inl"
+
+
+
