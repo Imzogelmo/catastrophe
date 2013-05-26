50c84e638a39f4afd1d65d3e66090ccc8ad9097b
arpeggiodragon <arpeggiodragon@a8cd0d33-2a72-5bf5-5b35-c9dba72e4cff>
2013-05-26 04:54:33 +0000

    ...oops - forgot to actually add GameData sources.

---
+++ trunk/Rpg/src/GameData.h
@@ -0,0 +1,55 @@
+// catastrophe rpg engine
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include <fc/string.h>
+#include <fc/vector.h>
+
+#include "Common.h"
+#include "Actor.h"
+#include "Party.h"
+
+
+class GameData
+{
+public:
+	// todo: Custom allocators for speed and fragmentation.
+	typedef fc::vector<Actor*>		actor_vec_type;
+	typedef fc::vector<Party*>		party_vec_type;
+
+	GameData();
+	~GameData();
+
+	//void SetDatabase( Database* database );
+	void InitializeFromDatabase( Database* database );
+
+	Actor* GetCharacterActor( int index );
+	Actor* GetMonsterActor( int index );
+	Actor* GetCharacterActorById( int id );
+	Actor* GetMonsterActorById( int id );
+
+	Party* GetParty( int id ) const;
+	Party* GetActiveParty() const;
+
+	Database* GetDatabase() const { return m_database; }
+
+private:
+	Database*			m_database;
+	party_vec_type		m_partyList;
+	actor_vec_type		m_characterActors;
+	actor_vec_type		m_monsterActors;
+
+	size_t				m_activeParty;
+
+};
+
+
