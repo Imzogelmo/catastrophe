de6d1577f51a59c93a7766a05b952dba2658b99c
arpeggiodragon <arpeggiodragon@a8cd0d33-2a72-5bf5-5b35-c9dba72e4cff>
2014-01-28 11:36:56 +0000

    Add cpp module files.

---
+++ Engine/src/MathModule.cpp
@@ -0,0 +1,36 @@
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+
+
+#include "Math/BoundingBox.inl"
+#include "Math/Color.inl"
+#include "Math/Colorf.inl"
+#include "Math/Frustum.inl"
+#include "Math/HSLColor.inl"
+#include "Math/HSVColor.inl"
+#include "Math/Math.inl"
+#include "Math/Matrix.inl"
+#include "Math/Quaternion.inl"
+#include "Math/Rect.inl"
+#include "Math/Rectf.inl"
+#include "Math/Vector2.inl"
+#include "Math/Vector3.inl"
+#include "Math/Vector4.inl"
+
+
